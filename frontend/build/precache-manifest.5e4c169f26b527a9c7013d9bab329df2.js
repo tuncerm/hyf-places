80e1a9f6fd7a4b73619d0cb0e1c73e4eaf08ea5e
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-16 01:26:55 +0100

    Deployin v1 to heroku

---
+++ frontend/build/precache-manifest.5e4c169f26b527a9c7013d9bab329df2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddaa293bb004fef50ba294e4ba31b9d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f8947c4dbb47d76b82e",
+    "url": "/static/css/0.a584a1b5.chunk.css"
+  },
+  {
+    "revision": "6c89e17939b033266098",
+    "url": "/static/css/4.ecd12a53.chunk.css"
+  },
+  {
+    "revision": "0cf0bd8fdc62b4288bc8",
+    "url": "/static/css/5.efd60c72.chunk.css"
+  },
+  {
+    "revision": "0f890ec6fcdc248a0b1d",
+    "url": "/static/css/6.19123ae6.chunk.css"
+  },
+  {
+    "revision": "84d92d27a1115ad6d775",
+    "url": "/static/css/7.73f75196.chunk.css"
+  },
+  {
+    "revision": "5e14da7cc0d238da675f",
+    "url": "/static/css/9.749363a7.chunk.css"
+  },
+  {
+    "revision": "855b54e46381b24ed46c",
+    "url": "/static/css/main.08b7a5b4.chunk.css"
+  },
+  {
+    "revision": "0f8947c4dbb47d76b82e",
+    "url": "/static/js/0.2b9c303c.chunk.js"
+  },
+  {
+    "revision": "28bacdc5eb69e1a7431d",
+    "url": "/static/js/3.8e6eeba5.chunk.js"
+  },
+  {
+    "revision": "6c89e17939b033266098",
+    "url": "/static/js/4.dfc5dc10.chunk.js"
+  },
+  {
+    "revision": "0cf0bd8fdc62b4288bc8",
+    "url": "/static/js/5.c335304d.chunk.js"
+  },
+  {
+    "revision": "0f890ec6fcdc248a0b1d",
+    "url": "/static/js/6.bea1f332.chunk.js"
+  },
+  {
+    "revision": "84d92d27a1115ad6d775",
+    "url": "/static/js/7.87e05a16.chunk.js"
+  },
+  {
+    "revision": "aabfd5452af8d11e8cdf",
+    "url": "/static/js/8.77b507a6.chunk.js"
+  },
+  {
+    "revision": "5e14da7cc0d238da675f",
+    "url": "/static/js/9.1df7cb8a.chunk.js"
+  },
+  {
+    "revision": "855b54e46381b24ed46c",
+    "url": "/static/js/main.366f656d.chunk.js"
+  },
+  {
+    "revision": "d40b7587f04d91c64788",
+    "url": "/static/js/runtime-main.930dd962.js"
+  }
+]);
